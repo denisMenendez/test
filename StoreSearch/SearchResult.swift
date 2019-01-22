75276df59c884c005e20191c1c3127da57a21007
Denis Menendez <denis.menendez@radmas.com>
2019-01-22 17:32:32 +0100

    Added a search bar

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,12 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Radmas Technologies on 22/01/2019.
+//  Copyright © 2019 Radmas Technologies. All rights reserved.
+//
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
