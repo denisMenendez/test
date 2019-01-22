b59986034feac2ed0a63a56ef2ed2ea8de98d66a
AppleDev <radmastech@mac-mini-de-radmas.local>
2019-01-22 10:03:49 +0100

    Initial Commit

---
+++ StoreSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StoreSearch
+//
+//  Created by Radmas Technologies on 22/01/2019.
+//  Copyright © 2019 Radmas Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
